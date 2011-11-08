f20d4ef19813ff1fb12fa4bfbe64fed97301b285
Tobias Crawley <tcrawley@gmail.com>
2011-11-07 21:49:55 -0500

    Bump version to 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-immutant "0.1.0"
+(defproject lein-immutant "0.2.0"
   :description "Leiningen plugin for managing an Immutant project."
   :url "https://github.com/immutant/lein-immutant"
   :mailing-list {:name "Immutant users list"
