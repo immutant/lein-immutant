76a059ebb5efabed51851ed36b842c19c49df528
Tobias Crawley <tcrawley@gmail.com>
2012-02-20 17:05:38 -0500

    Create the archive everytime if deploying with --archive to guarantee that the deps are resolved.
    This will go away when we move to lein2.

--- src/leiningen/immutant/archive.clj
+++ src/leiningen/immutant/archive.clj
@@ -5,11 +5,13 @@
 
 (defn archive
   "Creates an Immutant archive from the current project"
-  [project root]
-  ;; This will need to change for lein 2.0 - deps/deps currently
-  ;; resolves and copies the deps to lib/. In 2.0,
-  ;; it will just resolve the deps and do no copying.
-  (when (seq project)
-    (deps/deps project))
-  (let [jar-file (archive/create project root (System/getProperty "user.dir"))]
-    (println "Created" (.getAbsolutePath jar-file))))
+  ([project root]
+     (archive project root (System/getProperty "user.dir")))
+  ([project root dest-dir]
+     ;; This will need to change for lein 2.0 - deps/deps currently
+     ;; resolves and copies the deps to lib/. In 2.0,
+     ;; it will just resolve the deps and do no copying.
+     (when (seq project)
+       (deps/deps project))
+     (let [jar-file (archive/create project root dest-dir)]
+       (println "Created" (.getAbsolutePath jar-file)))))
--- src/leiningen/immutant/deploy.clj
+++ src/leiningen/immutant/deploy.clj
@@ -10,7 +10,9 @@
   [project root & args] 
   (let [jboss-home (common/get-jboss-home)
         deployed-file (if (some #{"--archive"} args)
-                        (deploy/deploy-archive jboss-home project root)
+                        (do
+                          (archive/archive project root root) ;; FIXME: this can go away once we move to lein2
+                          (deploy/deploy-archive jboss-home project root))
                         (deploy/deploy-dir jboss-home project root))]
     (println "Deployed" (util/app-name project root) "to" (.getAbsolutePath deployed-file))))
 
