fa33b91aa0fff6eedc843a7750ffa70a125ed545
Tobias Crawley <tcrawley@gmail.com>
2012-08-24 15:05:10 -0400

    Trigger a shutdown api call when exiting 'run'.

--- src/leiningen/immutant/run.clj
+++ src/leiningen/immutant/run.clj
@@ -1,10 +1,41 @@
 (ns leiningen.immutant.run
-  (:require [leiningen.immutant.common  :as common]
-            [leinjacker.eval            :as eval]
-            [immutant.deploy-tools.util :as util]))
+  (:require [clojure.java.io            :as io]
+            [leiningen.immutant.common  :as common]
+            [immutant.deploy-tools.util :as util]
+            [jboss-as.management        :as api]))
+
+
+(defn- pump
+  "Borrowed from leiningen.core/eval"
+  [reader out]
+  (let [buffer (make-array Character/TYPE 1000)]
+    (loop [len (.read reader buffer)]
+      (when-not (neg? len)
+        (.write out buffer 0 len)
+        (.flush out)
+        (Thread/sleep 100)
+        (recur (.read reader buffer))))))
+
+(defn- sh
+  "A version of clojure.java.shell/sh that streams out/err, and returns the process created.
+Borrowed from leiningen.core/eval and modified."
+  [& cmd]
+  (let [proc (.exec (Runtime/getRuntime)
+                    (into-array cmd))]
+    (.start (Thread. (bound-fn []
+                       (with-open [out (io/reader (.getInputStream proc))
+                                   err (io/reader (.getErrorStream proc))]
+                         (let [pump-out (doto (Thread. (bound-fn [] (pump out *out*))) .start)
+                               pump-err (doto (Thread. (bound-fn [] (pump err *err*))) .start)]
+                           (.join pump-out)
+                           (.join pump-err))))))
+    proc))
+
+(defn- add-shutdown-hook! [f]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
 
 (let [jboss-home (common/get-jboss-home)]
-  (defn standalone-sh []
+  (defn- standalone-sh []
     (str (.getAbsolutePath jboss-home) "/bin/standalone."
          (if common/windows? "bat" "sh")))
 
@@ -19,8 +50,6 @@
          (let [script (standalone-sh)
                params (replace {"--clustered" "--server-config=standalone-ha.xml"} opts)]
            (apply println "Starting Immutant:" script params)
-           (when common/windows?
-             (println "\n*********************************************************************************************************")
-             (println "NOTE: ^C currently won't cause Immutant to exit on Windows. You'll need to kill it with the Task Manager.")
-             (println "*********************************************************************************************************\n"))
-           (apply eval/sh script params))))))
+           (let [proc (apply sh script params)]
+             (add-shutdown-hook! api/shutdown)
+             (.waitFor proc)))))))
