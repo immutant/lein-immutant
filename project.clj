7fac9e14c96226e31216c32549547f9d923ae375
Tobias Crawley <tcrawley@gmail.com>
2012-09-04 12:11:46 -0400

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-immutant "0.10.0"
+(defproject lein-immutant "0.11.0-SNAPSHOT"
   :description "Leiningen plugin for managing an Immutant project."
   :url "https://github.com/immutant/lein-immutant"
   :mailing-list {:name "Immutant users list"
