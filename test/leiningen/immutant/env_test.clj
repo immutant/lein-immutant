7789ebd73f7afc3c7e584d102dffd9a9ae46fcea
Tobias Crawley <tcrawley@gmail.com>
2012-09-27 14:28:21 -0400

    :use test-helper instead of :requiring it to make things more readable.

--- test/leiningen/immutant/env_test.clj
+++ test/leiningen/immutant/env_test.clj
@@ -1,27 +1,27 @@
 (ns leiningen.immutant.env-test
-  (:use midje.sweet)
-  (:require [leiningen.immutant.test-helper :as h]
-            [clojure.java.io                :as io]))
+  (:use midje.sweet
+        leiningen.immutant.test-helper)
+  (:require [clojure.java.io :as io]))
 
-(h/setup-test-env)
+(setup-test-env)
 
 (defn run-lein-env [gen env & args]
-  (apply h/run-lein gen "immutant" "env"
+  (apply run-lein gen "immutant" "env"
          (conj (vec args)
                :dir "/tmp"
                :env env
                :return-result? true)))
 
-(h/for-all-generations
-  (println "\n==> Testing with lein generation" h/*generation*)
+(for-all-generations
+  (println "\n==> Testing with lein generation" *generation*)
 
   (facts "with IMMUTANT_HOME set"
     (let [alt-immutant-home (io/file (io/resource "alt-immutant-install"))
-          env (assoc h/base-lein-env
+          env (assoc base-lein-env
                 "IMMUTANT_HOME" (.getAbsolutePath alt-immutant-home))]
       
-      (fact (str "env should work for lein " h/*generation*)
-        (let [result (run-lein-env h/*generation* env)]
+      (fact (str "env should work for lein " *generation*)
+        (let [result (run-lein-env *generation* env)]
           (:exit result) => 0
           (:out result)  => (re-pattern
                              (str "immutant-home: "
@@ -32,16 +32,16 @@
                                   (.getAbsolutePath alt-immutant-home)
                                   "/jboss \\n"))))
       
-      (fact (str "env with an arg should work for lein " h/*generation*)
-        (let [result (run-lein-env h/*generation* env "immutant-home")]
+      (fact (str "env with an arg should work for lein " *generation*)
+        (let [result (run-lein-env *generation* env "immutant-home")]
           (:exit result) => 0
           (:out result)  => (.getAbsolutePath alt-immutant-home))
-        (let [result (run-lein-env h/*generation* env "jboss-home")]
+        (let [result (run-lein-env *generation* env "jboss-home")]
           (:exit result) => 0
           (:out result)  => (str (.getAbsolutePath alt-immutant-home) "/jboss")))
       
-      (fact (str "env with an invalid arg should work for lein " h/*generation*)
-        (let [result (run-lein-env h/*generation* env "ham")]
+      (fact (str "env with an invalid arg should work for lein " *generation*)
+        (let [result (run-lein-env *generation* env "ham")]
           (:exit result) => 0
           (:out result)  => ""
           (:err result)  => "ham is an unknown env key. Valid keys are: (immutant-home jboss-home)\n")))))
--- test/leiningen/immutant/init_test.clj
+++ test/leiningen/immutant/init_test.clj
@@ -1,43 +1,43 @@
 (ns leiningen.immutant.init-test
-  (:use midje.sweet)
-  (:require [leiningen.immutant.test-helper :as h]
-            [clojure.java.io                :as io]))
+  (:use midje.sweet
+        leiningen.immutant.test-helper)
+  (:require [clojure.java.io :as io]))
 
-(h/setup-test-env)
+(setup-test-env)
 
-(h/for-all-generations
-  (println "\n==> Testing with lein generation" h/*generation*)
+(for-all-generations
+  (println "\n==> Testing with lein generation" *generation*)
 
-  (let [project-name (str "testproj-" h/*generation*)]
+  (let [project-name (str "testproj-" *generation*)]
     
-    (fact (str "new should work for lein " h/*generation*)
-      (h/with-tmp-dir
-        (let [project-dir (io/file h/*tmp-dir* project-name)]
-          (h/run-lein h/*generation* "immutant" "new" project-name
-                      :dir h/*tmp-dir*
-                      :env h/base-lein-env) => 0
+    (fact (str "new should work for lein " *generation*)
+      (with-tmp-dir
+        (let [project-dir (io/file *tmp-dir* project-name)]
+          (run-lein *generation* "immutant" "new" project-name
+                      :dir *tmp-dir*
+                      :env base-lein-env) => 0
           (.exists (io/file project-dir)) => true
           (.exists (io/file project-dir "immutant.clj")) => true)))
 
-    (if (= 2 h/*generation*)
+    (if (= 2 *generation*)
       (fact "'new immutant' should work with lein 2"
-      (h/with-tmp-dir
+      (with-tmp-dir
         (let [project-name (str project-name "-new")
-              project-dir (io/file h/*tmp-dir* project-name)]
-          (h/run-lein h/*generation* "new" "immutant" project-name
-                      :dir h/*tmp-dir*
-                      :env h/base-lein-env) => 0
+              project-dir (io/file *tmp-dir* project-name)]
+          (run-lein *generation* "new" "immutant" project-name
+                      :dir *tmp-dir*
+                      :env base-lein-env) => 0
           (.exists (io/file project-dir)) => true
           (.exists (io/file project-dir "immutant.clj")) => true))))
     
-    (fact (str "init should work for lein " h/*generation*)
-      (h/with-tmp-dir
-        (let [project-dir (io/file h/*tmp-dir* project-name)]
-          (h/run-lein h/*generation* "new" project-name
-                      :dir h/*tmp-dir*
-                      :env h/base-lein-env)
-          (h/run-lein h/*generation* "immutant" "init"
+    (fact (str "init should work for lein " *generation*)
+      (with-tmp-dir
+        (let [project-dir (io/file *tmp-dir* project-name)]
+          (run-lein *generation* "new" project-name
+                      :dir *tmp-dir*
+                      :env base-lein-env)
+          (run-lein *generation* "immutant" "init"
                       :dir project-dir
-                      :env h/base-lein-env) => 0
+                      :env base-lein-env) => 0
           (.exists (io/file project-dir "immutant.clj")) => true)))))
 
