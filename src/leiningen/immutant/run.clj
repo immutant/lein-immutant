806171b247472c79cdde2af9c8467dfca96d5ad2
Tobias Crawley <tcrawley@gmail.com>
2012-10-28 19:30:49 -0400

    Abuse lein's trampoline to run standalone.sh directly [issue #4].

--- src/leiningen/immutant/run.clj
+++ src/leiningen/immutant/run.clj
@@ -1,34 +1,9 @@
 (ns leiningen.immutant.run
   (:require [clojure.java.io            :as io]
+            [clojure.string             :as str]
             [leiningen.immutant.common  :as common]
             [immutant.deploy-tools.util :as util]))
 
-
-(defn- pump
-  "Borrowed from leiningen.core/eval"
-  [reader out]
-  (let [buffer (make-array Character/TYPE 1000)]
-    (loop [len (.read reader buffer)]
-      (when-not (neg? len)
-        (.write out buffer 0 len)
-        (.flush out)
-        (Thread/sleep 100)
-        (recur (.read reader buffer))))))
-
-(defn- sh
-  "A version of clojure.java.shell/sh that streams out/err, and returns the process created.
-Borrowed from leiningen.core/eval and modified."
-  [& cmd]
-  (let [proc (.exec (Runtime/getRuntime)
-                    (into-array cmd))]
-    (.start (Thread. (bound-fn []
-                       (with-open [out (io/reader (.getInputStream proc))
-                                   err (io/reader (.getErrorStream proc))]
-                         (let [pump-out (doto (Thread. (bound-fn [] (pump out *out*))) .start)
-                               pump-err (doto (Thread. (bound-fn [] (pump err *err*))) .start)]
-                           (.join pump-out)
-                           (.join pump-err))))))
-    proc))
 
 (let [jboss-home (common/get-jboss-home)]
   (defn- standalone-sh []
@@ -44,8 +19,13 @@ Borrowed from leiningen.core/eval and modified."
        (util/with-jboss-home jboss-home
          (and project (not (util/application-is-deployed? project nil nil))
               (common/err "WARNING: The current app may not be deployed - deploy with 'lein immutant deploy'"))
-         (let [script (standalone-sh)
-               params (replace {"--clustered" "--server-config=standalone-ha.xml"} opts)]
-           (apply println "Starting Immutant:" script params)
-           (let [proc (apply sh script params)]
-             (.waitFor proc)))))))
+         (let [trampoline-file (System/getProperty "leiningen.trampoline-file")
+               params (replace {"--clustered" "--server-config=standalone-ha.xml"} opts)
+               command (concat [(standalone-sh)] params)]
+           (apply println "Starting Immutant:" command)
+           (.mkdirs (.getParentFile (io/file trampoline-file)))
+           (spit trampoline-file 
+                 (str/join " "
+                  (map #(format "\"%s\"" %)
+                       (replace {"--clustered" "--server-config=standalone-ha.xml"}
+                                command)))))))))
