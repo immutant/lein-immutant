bc4d3f5e206d19688e9318b63635c9e7b70dd442
Toby Crawley <toby@tcrawley.org>
2013-03-26 14:51:28 -0400

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-immutant "0.17.2-SNAPSHOT"
+(defproject lein-immutant "0.18.0-SNAPSHOT"
   :description "Leiningen plugin for managing an Immutant project."
   :url "https://github.com/immutant/lein-immutant"
   :mailing-list {:name "Immutant users list"
