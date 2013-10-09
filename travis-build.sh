00adc0398d7edcf8a523f0310966bffa7afa2908
Toby Crawley <toby@tcrawley.org>
2013-10-09 13:30:00 -0400

    A little more debugging.

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -f -L https://raw.github.com/technomancy/leiningen/2.3.2/bin/lein > ./lein
+curl -f -L https://raw.github.com/technomancy/leiningen/2.3.3/bin/lein > ./lein
 chmod +x ./lein
-./lein midje
+./lein version
+PRINT_OUT=1 ./lein midje
