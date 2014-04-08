dae3ccba97035852bd27adc1958641feb96706aa
Toby Crawley <toby@tcrawley.org>
2014-04-08 07:42:55 -0400

    Release 1.2.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-immutant "1.2.1-SNAPSHOT"
+(defproject lein-immutant "1.2.1"
   :description "Leiningen plugin for managing an Immutant project."
   :url "https://github.com/immutant/lein-immutant"
   :mailing-list {:name "Immutant users list"
