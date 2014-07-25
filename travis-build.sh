b3924719d8aa477924680fc8783df7a68c3c28a3
Toby Crawley <toby@tcrawley.org>
2014-07-24 20:16:18 -0600

    Test with latest lein version.

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -f -L https://raw.github.com/technomancy/leiningen/2.3.3/bin/lein > ./lein
+curl -f -L https://raw.github.com/technomancy/leiningen/2.4.2/bin/lein > ./lein
 chmod +x ./lein
 ./lein version
 export LEIN2_CMD=`pwd`/lein
-export PRINT_OUT=1 
+export PRINT_OUT=1
 ./lein midje
