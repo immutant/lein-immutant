e1dfe7b6198553f77074c7e458dfcbc92c395bce
Toby Crawley <toby@tcrawley.org>
2014-07-28 15:02:19 -0400

    First cut at a war task [#93]

--- src/leiningen/immutant.clj
+++ src/leiningen/immutant.clj
@@ -39,21 +39,25 @@
      (if (map? m)
        (concat (keys m) (mapcat (fn [[_ v]] (extract-keys acc v)) m)))))
 
+(defn- extract-deps [project]
+  (->> project
+    (cp/dependency-hierarchy :dependencies)
+    extract-keys))
+
 (defn- locate-version [project ns]
-  (if-let [version (->> project
-                     (cp/dependency-hierarchy :dependencies)
-                     extract-keys
-                     (some (fn [[dep version]]
-                             (if (= ns (namespace dep))
-                               version))))]
-      version
-      (abort (format "No %s dependency found in the project's dependency tree." ns))))
+  (if-let [version (some (fn [[dep version]]
+                           (if (= ns (namespace dep))
+                             version))
+                     (extract-deps project))]
+    version
+    (abort (format "No %s dependency found in the project's dependency tree." ns))))
 
 (defn- classpath [project]
   ;; TODO: don't displace a version that may be there
   (-> project
     (update-in [:dependencies]
-      conj ['org.immutant/wildfly (locate-version project "org.immutant")])
+      conj ['org.immutant/wildfly (locate-version project "org.immutant")
+            :exclusions ['org.projectodd.wunderboss/wunderboss-wildfly]])
     cp/get-classpath))
 
 (defn- build-descriptor [project uber?]
@@ -62,7 +66,7 @@
     (not uber?) (merge
                   {:root (:root project)
                    :classpath (str/join ":" (classpath project))
-                    "config.repl-options" (pr-str (:repl-options project))})))
+                   "config.repl-options" (pr-str (:repl-options project))})))
 
 (defn- map->properties [m]
   (reduce (fn [p [k v]]
@@ -71,49 +75,37 @@
     (Properties.)
     m))
 
-(defn- find-base-jars [project]
-  (let [version (locate-version project "org.projectodd.wunderboss")]
-    (filter #(re-find #"^wunderboss-.*\.jar$" (.getName %))
-      (cp/resolve-dependencies :dependencies
-        (update-in project [:dependencies]
-          conj ['org.projectodd.wunderboss/wunderboss-wildfly version])))))
-
-(defn- find-wildfly-jars [project uber?]
+(defn- find-all-wildfly-jars [project]
   (->> (assoc project
          :dependencies [['org.immutant/wildfly (locate-version project "org.immutant")
-                         :exclusions (cond->
-                                       ['org.immutant/core
-                                        'org.clojure/clojure
-                                        'org.projectodd.wunderboss/wunderboss-clojure]
-                                       uber? (conj 'org.clojure/tools.nrepl))]])
-    (cp/resolve-dependencies :dependencies)
-    #_(filter #(re-find #"wildfly-.*\.jar$" (.getName %)))
-    #_first))
+                         :exclusions
+                         ['org.immutant/core
+                          'org.clojure/clojure
+                          'org.projectodd.wunderboss/wunderboss-clojure]]])
+    (cp/resolve-dependencies :dependencies)))
 
-;; We need to keep the manifest that comes from the wunderboss-wildfly.jar
-;; a better solution is to probably read it as Properties and merge there
-(defn- merge-manifests [new prev]
-  (if (re-find #"Dependencies" new)
-    new
-    prev))
+(defn- relevant-wboss-dependencies [project]
+  (let [deps (mapv first (extract-deps project))
+        immutant-deps (filter #(= "org.immutant" (namespace %)) deps)
+        match #(some #{'org.immutant/immutant %} immutant-deps)]
+    (cond-> ['org.projectodd.wunderboss/wunderboss-wildfly]
+      (match 'org.immutant/caching)   (conj 'org.projectodd.wunderboss/wunderboss-caching)
+      (match 'org.immutant/messaging) (conj 'org.projectodd.wunderboss/wunderboss-messaging)
+      (match 'org.immutant/web)       (conj 'org.projectodd.wunderboss/wunderboss-web))))
 
-(defn- build-jar [project to uber?]
-  ;; TODO: implement uber?
-  (with-open [out (-> to FileOutputStream. ZipOutputStream.)]
-    (uberjar/write-components (update-in project [:uberjar-merge-with]
-                                assoc
-                                #"(?i)^META-INF/MANIFEST.MF$"
-                                `[slurp #'leiningen.immutant/merge-manifests spit])
-      (->> project find-base-jars (sort-by #(.getName %))) out)))
+(defn- relevant-wboss-jars [project]
+  (let [wboss-version (locate-version project "org.projectodd.wunderboss")]
+    (->> (assoc project
+           :dependencies (mapv (fn [dep]
+                                 [dep wboss-version])
+                           (relevant-wboss-dependencies project)))
+      (cp/resolve-dependencies :dependencies))))
 
 (defn- build-war [project dest entries]
   (with-open [out (-> dest FileOutputStream. BufferedOutputStream. JarOutputStream.)]
     (doseq [[path content] entries]
       (.putNextEntry out (JarEntry. path))
       (io/copy content out))))
-
-(defn devwar [project path]
-  (println "Not yet implemented, sorry!"))
 
 (defn resolve-path [project path]
   (if path
@@ -128,8 +120,30 @@
         path-dir))
     (io/file (:target-path project))))
 
+(defn war [project path]
+  (let [resolved-path (resolve-path project path)
+        war-entries
+        (concat
+          (mapv (fn [jar]
+                  [(str "WEB-INF/lib/" (.getName jar))
+                   jar])
+            (relevant-wboss-jars project))
+          [["META-INF/app.properties"
+            (with-out-str
+              (-> project
+                (build-descriptor false)
+                map->properties
+                (.store *out* "")))]
+           ["WEB-INF/web.xml"
+            (slurp (io/resource "web.xml"))]])
+        file (io/file resolved-path (format "%s-dev.war" (:name project)))]
+    (println "Creating" (.getAbsolutePath file))
+    (build-war project file war-entries)
+    file))
+
 (defn uberwar [project path]
-  (let [war-entries
+  (let [resolved-path (resolve-path project path)
+        war-entries
         (concat
           [[(format "WEB-INF/lib/%s.jar" (:name project))
             (io/file (uberjar/uberjar project))]
@@ -143,18 +157,18 @@
             (slurp (io/resource "web.xml"))]]
           (mapv (fn [f] [(format "WEB-INF/lib/%s" (.getName f))
                         f])
-            (find-wildfly-jars project true)))
-        file (io/file (resolve-path project path) (format "%s.war" (:name project)))]
+            (find-all-wildfly-jars project)))
+        file (io/file resolved-path (format "%s.war" (:name project)))]
     (println "Creating" (.getAbsolutePath file))
     (build-war project file war-entries)
     file))
 
 (defn immutant
   "Manage the deployment lifecycle of an Immutant application."
-  {:subtasks [#'uberwar #'devwar]}
+  {:subtasks [#'uberwar #'war]}
   ([project subtask & [path]]
      (case subtask
-       "devwar"  (devwar project path)
+       "war"  (war project path)
        "uberwar" (uberwar project path)
        ;;TODO: print help for default
        )
